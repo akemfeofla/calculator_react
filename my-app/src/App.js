f99be28bec7f37796a0e4f5363cb90af7a982a4a
[SW7] 강동훈 <2023danielk@gmail.com>
2023-11-15 12:55:26 +0900

    compiled with problems

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './calculator/calculator.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <InputBox />
+      <Buttons>
+        <OperatorBtn value={'+'}>+</OperatorBtn>
+        <OperatorBtn value={'-'}>-</OperatorBtn>
+        <OperatorBtn value={'*'}>*</OperatorBtn>
+        <OperatorBtn value={'/'}>/</OperatorBtn>
+        <NumberBtn value={7}>7</NumberBtn>
+        <NumberBtn value={8}>8</NumberBtn>
+        <NumberBtn value={9}>9</NumberBtn>
+        <OperatorBtn value={'C'}>C</OperatorBtn>
+        <NumberBtn value={4}>4</NumberBtn>
+        <NumberBtn value={5}>5</NumberBtn>
+        <NumberBtn value={6}>6</NumberBtn>
+        <NumberBtn value={1}>1</NumberBtn>
+        <NumberBtn value={2}>2</NumberBtn>
+        <NumberBtn value={3}>3</NumberBtn>
+        <NumberBtn value={0}>0</NumberBtn>
+        <OperatorBtn value={'.'}>.</OperatorBtn>
+        <OperatorBtn value={'='}>=</OperatorBtn>
+      </Buttons>
+    </MainContainer>
+  
+  //컴포넌트 생성
+    const Welcome =(props)=>{
+      return <h2>Hello, {props.name}</h2>
+      
+      // 컴포넌트 사용
+      const App = ()=>{
+        return <div>
+          <Welcome name = '수영'/>
+          <Welcome name = '수영'/>
+          <Welcome name = '수영'/>
+        </div>
+      }
+    }
   );
 }
 
---
+++ my-app/src/Calculator/calculator.jsx
@@ -0,0 +1,30 @@
+
+
+//
+let value = 0
+
+function click(){
+    value++
+}
+
+function plusOperator() {
+  const element = (
+    <div>
+      <div>+</div>
+    </div>
+  );
+}
+
+setInterval(tick, 1000);
+
+const d = [0,1,2,3,4,5]
+
+const newD = d.map(function(item){
+return item*2
+})
+
+const a = [2,4,6,8]
+
+const newA = a.forEach(function(item){
+    console.log(item)
+}
